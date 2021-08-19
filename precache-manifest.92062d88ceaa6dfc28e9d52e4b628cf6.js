6b2661dcfeda188153861c7e73db420eb5840277
Gja <9206150+gja_001@user.noreply.gitee.com>
2021-08-19 10:56:27 +0800

    deploy

---
+++ precache-manifest.92062d88ceaa6dfc28e9d52e4b628cf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18624a0264e7d6d05c881497287bf245",
+    "url": "/svmoneysys-react-website/index.html"
+  },
+  {
+    "revision": "cdcab78d0a3adfb4c323",
+    "url": "/svmoneysys-react-website/static/css/main.fc6f2594.chunk.css"
+  },
+  {
+    "revision": "c612a0702ce5d645be23",
+    "url": "/svmoneysys-react-website/static/js/2.9626cdd2.chunk.js"
+  },
+  {
+    "revision": "de7c95d42723c2d4f76d99ad9a65a751",
+    "url": "/svmoneysys-react-website/static/js/2.9626cdd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdcab78d0a3adfb4c323",
+    "url": "/svmoneysys-react-website/static/js/main.59ba053b.chunk.js"
+  },
+  {
+    "revision": "5853f97ee6126edc8027",
+    "url": "/svmoneysys-react-website/static/js/runtime-main.7312ae1e.js"
+  }
+]);
